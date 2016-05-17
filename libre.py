8dbb6d81cf4f485c524d2ce987020edcc860e696
Kenia Cabrera <kenia.class@gmail.com>
2016-05-17 16:04:34 -0430

    Primera Prueba

---
+++ libre.py
@@ -0,0 +1,14 @@
+print "ingrese su nombre: "
+a = raw_input()
+print "ingrese su edad: "
+b = raw_input()
+print "ingrese su correo: "
+c = raw_input()
+print "ingrese carrera cursada: "
+d = raw_input()
+
+
+print "mi nombre es: ", a
+print "mi edad es: ", b
+print "mi correo es: ", c
+print "actualmente estudio: ", d
---
+++ uno.py
@@ -0,0 +1,22 @@
+#realice un algoritmo que permita el ingreso 
+#de datos por tecladoque evalue dicha entrada 
+#separe la entrada en numeros y letras e imprimalos por pantalla
+
+
+
+r = 0
+lista = [5] 
+while r <= 5
+		
+	x = raw_input ("ingrese caracter o numero: ")
+	lista.apend(x)
+	r = raw_input ("desea ingresar otro caracter? s/n: ") 
+		
+print lista
+		
+	
+	
+	
+
+
+separar(x)  
